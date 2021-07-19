895efee06f88aefd2a55ff3032fefd59fdb9e7f3
sharafas-om <sharafasom@gmail.com>
2021-07-19 20:57:11 +0530

    updates

---
+++ CrashUtil/src/main/java/com/oms/crashutil/activity/DefaultErrorActivity.java
@@ -0,0 +1,117 @@
+package com.oms.crashutil.activity;
+
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.DialogInterface;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.oms.crashutil.CustomActivityOnCrash;
+import com.oms.crashutil.R;
+import com.oms.crashutil.config.CrashUtilConfig;
+
+public final class DefaultErrorActivity extends AppCompatActivity {
+
+    @SuppressLint("PrivateResource")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        TypedArray a = obtainStyledAttributes(R.styleable.AppCompatTheme);
+        if (!a.hasValue(R.styleable.AppCompatTheme_windowActionBar)) {
+            setTheme(R.style.Theme_AppCompat_Light_DarkActionBar);
+        }
+        a.recycle();
+
+        setContentView(R.layout.crash_util_default_error_activity);
+
+
+        Button restartButton = findViewById(R.id.crash_util_error_activity_restart_button);
+
+        final CrashUtilConfig config = CustomActivityOnCrash.getConfigFromIntent(getIntent());
+
+        if (config == null) {
+
+            finish();
+            return;
+        }
+
+        if (config.isShowRestartButton() && config.getRestartActivityClass() != null) {
+            restartButton.setText(R.string.crash_util_error_activity_restart_app);
+            restartButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CustomActivityOnCrash.restartApplication(DefaultErrorActivity.this, config);
+                }
+            });
+        } else {
+            restartButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CustomActivityOnCrash.closeApplication(DefaultErrorActivity.this, config);
+                }
+            });
+        }
+
+        Button moreInfoButton = findViewById(R.id.crash_util_error_activity_more_info_button);
+
+        if (config.isShowErrorDetails()) {
+            moreInfoButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AlertDialog dialog = new AlertDialog.Builder(DefaultErrorActivity.this)
+                            .setTitle(R.string.crash_util_error_activity_error_details_title)
+                            .setMessage(CustomActivityOnCrash.getAllErrorDetailsFromIntent(DefaultErrorActivity.this, getIntent()))
+                            .setPositiveButton(R.string.crash_util_error_activity_error_details_close, null)
+                            .setNeutralButton(R.string.crash_util_error_activity_error_details_copy,
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            copyErrorToClipboard();
+                                        }
+                                    })
+                            .show();
+                    TextView textView = dialog.findViewById(android.R.id.message);
+                    if (textView != null) {
+                        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.crash_util_error_activity_error_details_text_size));
+                    }
+                }
+            });
+        } else {
+            moreInfoButton.setVisibility(View.GONE);
+        }
+
+        Integer defaultErrorActivityDrawableId = config.getErrorDrawable();
+        ImageView errorImageView = findViewById(R.id.crash_util_error_activity_image);
+
+        if (defaultErrorActivityDrawableId != null) {
+            errorImageView.setImageDrawable(ResourcesCompat.getDrawable(getResources(), defaultErrorActivityDrawableId, getTheme()));
+        }
+    }
+
+    private void copyErrorToClipboard() {
+        String errorInformation = CustomActivityOnCrash.getAllErrorDetailsFromIntent(DefaultErrorActivity.this, getIntent());
+
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+
+        if (clipboard != null) {
+            ClipData clip = ClipData.newPlainText(getString(R.string.crash_util_error_activity_error_details_clipboard_label), errorInformation);
+            clipboard.setPrimaryClip(clip);
+            Toast.makeText(DefaultErrorActivity.this, R.string.crash_util_error_activity_error_details_copied, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
